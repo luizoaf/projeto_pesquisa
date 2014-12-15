463d79ad0c8df9900d33c4f16b5a4f711a467943
Luiz <luizoaf@gmail.com>
2014-12-15 12:19:18 -0300

    Tudo que foi feito desde agosto de 2014

---
+++ 4 - calculo coeficiente B e volatilidade por setor em cada semestre/4_erro_coeficiente_B.R
@@ -0,0 +1,16 @@
+serie_retornos_frequencia = funcao_distribuicao_probabilidade(serie)$frequencia_eixo_y
+eixo_x_frequencias = funcao_distribuicao_probabilidade(serie)$valor_serie_retorno_eixo_x
+exponencial = resultado_funcao_exponencial(eixo_x_frequencias)
+volatilidade = calcula_volatilidade(serie)
+
+# tamanho_serie = length(serie_retornos_frequencia)
+exponencial_serie_retornos_frequencia_norm = data.frame(exponencial=exponencial,serie_retornos_frequencia = serie_retornos_frequencia)
+exponencial_serie_retornos_frequencia_norm = normalizacao_transformacao_linear(0.15,0.85,exponencial_serie_retornos_frequencia_norm)
+mape = sum(abs(exponencial_serie_retornos_frequencia_norm$exponencial-exponencial_serie_retornos_frequencia_norm$serie_retornos_frequencia)/exponencial_serie_retornos_frequencia_norm$serie_retornos_frequencia)/length(exponencial)
+
+# mean(100*abs(exponencial_serie_retornos_frequencia_norm$exponencial-exponencial_serie_retornos_frequencia_norm$serie_retornos_frequencia)/exponencial_serie_retornos_frequencia_norm$serie_retornos_frequencia)
+# sse = sum(( exponencial-serie_retornos_frequencia)^2)
+sse = sum(( exponencial_serie_retornos_frequencia_norm$exponencial-exponencial_serie_retornos_frequencia_norm$serie_retornos_frequencia)^2)
+rmse = sqrt(mean((exponencial_serie_retornos_frequencia_norm$exponencial-exponencial_serie_retornos_frequencia_norm$serie_retornos_frequencia)^2))
+
+
