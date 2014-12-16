dddbba453204fdcb160b643fe4bd85f0adef1ff3
Luiz <luizoaf@gmail.com>
2014-12-15 23:48:31 -0300

    Cálculo do a(variável que multiplica a função exponencial) funcionando para várias casas decimais. Precisa refatorar

--- 4 - calculo coeficiente B e volatilidade por setor em cada semestre/3_define_coeficiente_B.R
+++ 4 - calculo coeficiente B e volatilidade por setor em cada semestre/3_define_coeficiente_B.R
@@ -11,15 +11,98 @@ incremento_atual = incremento[1]
 coeficiente_B_e_erros = data.frame()
 transicao = F
 
+
+# # a : ajudar no calculo
+# incremento_a = c(0.5,0.1,0.05,0.01,0.005,0.001,0.0001)
+# i_incremento_a = length(incremento_a)
+# a = incremento_a[i_incremento_a]
+# i_erros_a =1
+# a_atual = incremento_a[i_incremento_a]
+incremento_a = c(0.5,0.1,0.05,0.01,0.005,0.001,0.0001)
 while(T){
   valores_a = data.frame()
+  #   
+  #   for(a in seq(from=0.1,to = 1.5,by=0.1)){
+  #     previsao = resultado_funcao_exponencial(a,eixo_x_frequencias,coeficiente_B)
+  #     sse = sum(( previsao-alvo)^2)
+  #     valores_a = rbind(valores_a,c(a,sse))
+  #   }
   
-  for(a in seq(from=0.1,to = 1.5,by=0.1)){
+  # a : ajudar no calculo
+  i_incremento_a = 1
+  a = incremento_a[length(incremento_a)]
+  a_atual = incremento_a[1]
+  i_erros_a =1
+  transicao_a = F
+  #   chegou_no_max= FALSE
+  while(T){
     previsao = resultado_funcao_exponencial(a,eixo_x_frequencias,coeficiente_B)
     sse = sum(( previsao-alvo)^2)
     valores_a = rbind(valores_a,c(a,sse))
+    colnames(valores_a) = c("a","sse")
+    valores_a
+    if(i_erros_a>2 && valores_a$sse[i_erros_a-1] >= valores_a$sse[i_erros_a] && valores_a$a > 0){
+      a_atual = incremento_a[i_incremento_a]
+      if(transicao){
+        a = a - ifelse(is.na(a_atual),0,a_atual)
+      }else{
+        a = a + ifelse(is.na(a_atual),0,a_atual)
+      }
+    }else{
+      i_incremento_a = i_incremento_a + 1
+      transicao_a = !transicao_a
+      a_atual = incremento_a[i_incremento_a]
+      if(transicao_a){
+        a = a - ifelse(is.na(a_atual),0,a_atual)
+      }else{
+        a = a + ifelse(is.na(a_atual),0,a_atual)
+      }
+    }
+    
+    if(i_erros_a==1){
+      a_atual = incremento_a[i_incremento_a]
+      a = a + ifelse(is.na(a_atual),0,a_atual)
+    }
+    if(is.na(a_atual)){
+      break
+    }
+    i_erros_a = i_erros_a + 1
+    #     #     a = i_incremento_a_atual
+    #     #     a = a_atual
+    #     previsao = resultado_funcao_exponencial(a,eixo_x_frequencias,coeficiente_B)
+    #     sse = sum(( previsao-alvo)^2)
+    #     valores_a = rbind(valores_a,c(a,sse))
+    #     colnames(valores_a) = c("a","sse")
+    #     valores_a
+    #     if(a == incremento_a[i_incremento_a]){
+    #       i_incremento_a = i_incremento_a - 1
+    #       a = incremento_a[i_incremento_a]
+    #       #       coeficiente_B = coeficiente_B + ifelse(is.na(incremento_atual),0,incremento_atual)
+    #     }else{
+    #       if(valores_a$sse[i_erros_a-1] > valores_a$sse[i_erros_a] && chegou_no_max == FALSE){
+    #         i_incremento_a = i_incremento_a - 1
+    #         a =  incremento_a[i_incremento_a]
+    #       }else{
+    #         chegou_no_max = !chegou_no_max
+    #         i_incremento_a = i_incremento_a + 1
+    #         a = incremento_a[i_incremento_a]
+    #         
+    #       }
+    #     }
+    #     #     else{
+    #     #       if(valores_a$sse[i_erros_a-1] < valores_a$sse[i_erros_a]){
+    #     #         i_incremento_a = i_incremento_a + 1
+    #     #         a_atual = incremento_a[i_incremento_a]
+    #     #       }else{
+    #     #         a_atual = a_atual+ incremento_a[i_incremento_a]
+    #     #       }
+    #     #     }
+    #     #     if(is.na(a_atual)){
+    #     #       break
+    #     #     }
+    #     i_erros_a = i_erros_a + 1
   }
-  colnames(valores_a) = c("a","sse")
+  #   colnames(valores_a) = c("a","sse")
   menor_sse = valores_a$sse[which.min(valores_a$sse)]
   coeficiente_B_e_erros_menor_sse = subset(valores_a,valores_a$sse == menor_sse)
   a = coeficiente_B_e_erros_menor_sse$a
